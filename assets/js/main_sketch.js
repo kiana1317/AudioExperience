5e69ec8b2145d2de1054969e8176a5042930a573
kianah <kianah@uchicago.edu>
2021-05-04 02:03:20 -0500

    Working Sample

--- assets/js/main_sketch.js
+++ assets/js/main_sketch.js
@@ -1,68 +1,127 @@
 // Teachable Machine
 
-// Storing the label
-let label = "waiting...";
+// Storing the labels
+let labels_dict = {};
+let labels_list = [];
+
+// Number of sketches
+let num_of_classifiers = 9;
+
+let sample_classifier = "rave";
+
+let alpha_bound = 255 / num_of_classifiers - 1;
+
 
 // Classifier and model url
 let classifier;
 
 let modelURL = 'https://teachablemachine.withgoogle.com/models/sZjfSVJTi/';
 
-// STEP 1: Load the model!
+// Teachable Machine Model Load
 function preload() {
-  classifier = ml5.soundClassifier(modelURL + 'model.json');
+    classifier = ml5.soundClassifier(modelURL + 'model.json');
 }
 
-let snowflakes = []; 
-
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(window.innerWidth, window.innerHeight);
 
-  textSize(width / 30);
-  textAlign(CENTER, CENTER);
-  // STEP 2: Start classifying (will listen to mic by default)
-  classifyAudio();
+    // STEP 2: Start classifying (will listen to mic by default)
+    classifyAudio();
 }
 
 // STEP 2 classify!
 function classifyAudio() {
-  classifier.classify(gotResults);
+    classifier.classify(gotResults);
 }
 
 function draw() {
-  background(220);
+    background(200);
 
-  // STEP 4: Draw the label
-  //text(label, width/2, height - 16);
-  // Background noise is headphones
-  // Draw the emoji
-  // textSize(256);
-  // text(label, width / 2, height / 2);
+    // Wait until the classifier starts to begin running sketches
+    print(labels_dict)
+    if (labels_dict[sample_classifier] == null){
+        console.log("Not executing");
+        return;
+    }
 
-  let original = 'start';
-  if (label == 'Background Noise') {
+    // Call each sketch
 
-  } else if (label == 'Black Hole') {
-    background(0);
-  } else if (label == 'banjo') {
-    fill(255);
-    push();
-  translate(width * 0.5, height * 0.5);
-  rotate(frameCount / 50.0);
-  star(0, 0, 80, 100, 40);
-  pop();
+    // blackHole();
+    // banjo();
+    // birdSong();
+    // ding();
+    // guitarStrum();
+    // percussive();
+    if (labels_dict["rain"] < alpha_bound){
+        rain();
+    }
+  
+    if (labels_dict["rave"] < alpha_bound){
+        rave();
+    }
+    // wishYouWereHere();
+}
 
-  } else if (label == 'bird song') {
-    background(200,50,40);
-  } else if (label == 'ding') {
-    fill(177, 166, 55);
-    rect(50, 60, 70, 80);
-  } else if (label == 'guitar strum') {
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    // Store the labels
+    labels_tm = results;
 
-  } else if (label == 'percussion') {
+    for (let a = 0; a < labels_tm.length ; a += 1) { 
+        labels_dict[labels_tm[a].label]=labels_tm[a].confidence * 255; 
+    }
 
-  } else if (label == 'rain') {
-    background('brown');
+}
+
+
+///////////////////////////////////////////////////////////////////
+// Black Hole Sketch
+function blackHole(){
+    background(50);
+}
+
+///////////////////////////////////////////////////////////////////
+// Banjo Sketch
+function banjo(){
+    background(100);
+}
+
+///////////////////////////////////////////////////////////////////
+// Bird Song Sketch
+function birdSong(){
+    background(150);
+}
+
+///////////////////////////////////////////////////////////////////
+// Ding Sketch
+function ding(){
+    background(200);
+}
+
+///////////////////////////////////////////////////////////////////
+// Guitar Strum Sketch
+function guitarStrum(){
+    background(10);
+}
+
+///////////////////////////////////////////////////////////////////
+// Percussion Sketch
+function percussive(){
+    background(225);
+}
+
+///////////////////////////////////////////////////////////////////
+// Rain Sketch
+
+
+let snowflakes = []; // array to hold snowflake objects
+
+function rain() {
+  noStroke();
   let t = frameCount / 60; // update time
 
   // create a random number of snowflakes each frame
@@ -75,28 +134,9 @@ function draw() {
     flake.update(t); // update snowflake position
     flake.display(); // draw snowflake
   }
-  } else if (label == 'rave') {
-
-  } else if (label == 'wish you were here') {
-
-  }
 }
 
-function star(x, y, radius1, radius2, npoints) {
-  let angle = TWO_PI / npoints;
-  let halfAngle = angle / 2.0;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius2;
-    let sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
-}
-
+// snowflake class
 function snowflake() {
   // initialize coordinates
   this.posX = 0;
@@ -125,170 +165,61 @@ function snowflake() {
   };
 
   this.display = function() {
+    fill(50,200);
     ellipse(this.posX, this.posY, this.size);
   };
 }
 
-// STEP 3: Get the classification!
-function gotResults(error, results) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  // Store the label
-  label = results[0].label;
+///////////////////////////////////////////////////////////////////
+// Rave Sketch
+let cirx;
+let d;
+let row;
+let r, g, b;
+
+function rave(){
+    // This is the information that would go in the design
+    row = 0;
+    inc = 75;
+
+    // Setting the alpha (transparency) for the design
+    alpha = labels_dict["rave"];
+
+    // the sketch
+    for (let y = -5; y <= window.innerHeight + 180; y += 30) {
+
+        if (row % 2 != 1) {
+        cirx = 0;
+        } else {
+        cirx = 84;
+        }
+
+        for (let x = cirx; x <= window.innerWidth; x += 170) {
+
+            for (let icircle = 9; icircle > 0; icircle--) {
+                if (icircle % 2 == 0) {
+
+                r = mouseX - (row * 10);
+                g = (row * 10);
+                b = (row * 20);
+
+                fill(r, g, b, alpha);
+                 //increase diameter of each circle by 20px
+                 d = icircle * 30;
+
+                 //draw circle 1
+                 circle(x, y, d);
+
+                } 
+            }
+        }
+        
+        row++
+    }
 }
 
+///////////////////////////////////////////////////////////////////
+// Wish You Were Here Sketch
+function wishYouWereHere(){
 
-
-
-
-
-
-
-
-
-// // Teachable Machine
-
-// // Storing the label
-// let label = "waiting...";
-
-// // Classifier and model url
-// let classifier;
-
-// let modelURL = 'https://teachablemachine.withgoogle.com/models/sZjfSVJTi/';
-
-// let labels= {};
-// // STEP 1: Load the model!
-// function preload() {
-//   classifier = ml5.soundClassifier(modelURL + 'model.json');
-// }
-
-// let snowflakes = []; 
-
-// function setup() {
-//   createCanvas(800, 600);
-
-//   textSize(width / 30);
-//   textAlign(CENTER, CENTER);
-//   // STEP 2: Start classifying (will listen to mic by default)
-//   classifyAudio();
-// }
-
-// // STEP 2 classify!
-// function classifyAudio() {
-//   classifier.classify(gotResults);
-// }
-
-// function draw() {
-//   background(220);
-
-//   // STEP 4: Draw the label
-//   //text(label, width/2, height - 16);
-//   // Background noise is headphones
-//   // Draw the emoji
-//   // textSize(256);
-//   // text(label, width / 2, height / 2);
-
-//   let original = 'start';
-//   if (label == 'Background Noise') {
-
-//   } else if (label == 'Black Hole') {
-//     background(0);
-//   } else if (label == 'banjo') {
-//     fill(255);
-//     push();
-//   translate(width * 0.5, height * 0.5);
-//   rotate(frameCount / 50.0);
-//   star(0, 0, 80, 100, 40);
-//   pop();
-
-  
-//     if (labels["rave"] > 0.1){
-//         rain();
-//     }
-//   else if (label == 'rave') {
-
-//   } else if (label == 'wish you were here') {
-
-//   }
-// }
-// }
-// // 
-// function rain(){
-//    background('brown');
-//     let t = frameCount / 60; // update time
-
-//     // create a random number of snowflakes each frame
-//     for (let i = 0; i < random(5); i++) {
-//       snowflakes.push(new snowflake()); // append snowflake object
-//     }
-
-//     // loop through snowflakes with a for..of loop
-//     for (let flake of snowflakes) {
-//       flake.update(t); // update snowflake position
-//       flake.display(); // draw snowflake
-//     }
-// }
-
-// function star(x, y, radius1, radius2, npoints) {
-//   let angle = TWO_PI / npoints;
-//   let halfAngle = angle / 2.0;
-//   beginShape();
-//   for (let a = 0; a < TWO_PI; a += angle) {
-//     let sx = x + cos(a) * radius2;
-//     let sy = y + sin(a) * radius2;
-//     vertex(sx, sy);
-//     sx = x + cos(a + halfAngle) * radius1;
-//     sy = y + sin(a + halfAngle) * radius1;
-//     vertex(sx, sy);
-//   }
-//   endShape(CLOSE);
-// }
-
-// function snowflake() {
-//   // initialize coordinates
-//   this.posX = 0;
-//   this.posY = random(-50, 0);
-//   this.initialangle = random(0, 2 * PI);
-//   this.size = random(2, 5);
-
-//   // radius of snowflake spiral
-//   // chosen so the snowflakes are uniformly spread out in area
-//   this.radius = sqrt(random(pow(width / 2, 2)));
-
-//   this.update = function(time) {
-//     // x position follows a circle
-//     let w = 0.6; // angular speed
-//     let angle = w * time + this.initialangle;
-//     this.posX = width / 2 + this.radius * sin(angle);
-
-//     // different size snowflakes fall at slightly different y speeds
-//     this.posY += pow(this.size, 0.5);
-
-//     // delete snowflake if past end of screen
-//     if (this.posY > height) {
-//       let index = snowflakes.indexOf(this);
-//       snowflakes.splice(index, 1);
-//     }
-//   };
-
-//   this.display = function() {
-//     ellipse(this.posX, this.posY, this.size);
-//   };
-// }
-
-// // STEP 3: Get the classification!
-// function gotResults(error, results) {
-//   if (error) {
-//     console.error(error);
-//     return;
-//   }
-//   // Store the label
-//   labels_tm = results;
-  
-//   for (let a = 0; a < labels.length ; a += 1) { 
-//     labels[labels_tm[a].label]=labels_tm[a].confidence; 
-//   }
-  
-// }
+}
